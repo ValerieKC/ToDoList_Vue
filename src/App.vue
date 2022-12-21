1c6583154e0f1bfa137cc81fc2ad7e8e3c437db3
Kuan-Chen Valerie Wang <valerie81.wang@gmail.com>
2022-12-22 03:04:23 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+   import ToDoVue from './components/ToDoVue.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+  </header>
+
+  <main>
+    <ToDoVue />
+  </main>
+</template>
+
+<style scoped>
+  header {
+    line-height: 1.5;
+  }
+
+  .logo {
+    display: block;
+    margin: 0 auto 2rem;
+  }
+
+</style>
